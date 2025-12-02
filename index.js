319bc9a5e82b9346a34bf707944f9226b61870e1
beratozlu42 <yusufberatozlu@gmail.com>
2025-12-02 18:23:25 +0200

    Images change
    - Empty images changed with placeholder.
    - Some products has been removed.

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const productsRoute = require('./routes/products.js');
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: "http://localhost:5173" //CORS FOR REACT!
-}));
+app.use(cors());
 
 app.use(express.json());
 
@@ -18,6 +16,6 @@ app.get('/', (req, res) => {
     res.send('Backend is running...');
 });
 
-app.listen(PORT, () => {
-    console.log(`Running on: http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on 3000");
 });
